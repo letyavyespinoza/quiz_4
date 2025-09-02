93d7617f4e0c2a39dafcf8105d4b4f0fc7c9c53d
letyavyespinoza <letyavy.jo.espinozarivas@gmail.com>
2025-09-02 13:59:33 -0600

    quiz 4 intento 10

---
+++ quiz_4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+/**
+ *
+ * @author USER
+ */
+public class quiz_4 {
+
+    public void quiz_4() {
+        System.out.println("Hi");
+    }
+    
+    
+}
